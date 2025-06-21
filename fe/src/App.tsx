ef283ce6438573b9292d470174de966e885e1982
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-06-21 17:05:41 +0530

    fe setup

---
+++ fe/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <div className="bg-gray-800 w-full min-h-screen flex items-center justify-center">
+        <h1 className="text-yellow-500 text-9xl font-bold">Notekey Frontend</h1>
+      </div>
+    </>
+  );
+}
