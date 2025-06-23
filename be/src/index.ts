727065afbcdbe82b5539657388af74219658c29f
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-06-23 13:29:50 +0530

    cors update

--- be/src/index.ts
+++ be/src/index.ts
@@ -2,16 +2,27 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import apiRouter from "./routes";
-import "./models"
+import "./models";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.port ?? 3001;
-app.use(cors());
+
+// ✅ Proper CORS setup
+app.use(
+  cors({
+    origin: [
+      "https://notekey-duck.vercel.app/",
+      "https://notekey-7pftltf16-venomusblood568s-projects.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-app.use("/api",apiRouter);
+app.use("/api", apiRouter);
 
-app.listen(PORT, ()=> {
-    console.log(`Server running on http://localhost:${PORT}/`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/`);
+});
